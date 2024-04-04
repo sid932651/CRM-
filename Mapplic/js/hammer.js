ceedbaae26ab8aa3d39ff9c9edbd930565b383a1
sid932651 <163431762+sid932651@users.noreply.github.com>
2024-04-04 20:44:12 +0530

    Update hammer.js

--- Mapplic/js/hammer.js
+++ Mapplic/js/hammer.js
@@ -1,8 +1,4 @@
-/*! Hammer.JS - v1.0.9 - 2014-03-18
- * http://eightmedia.github.com/hammer.js
- *
- * Copyright (c) 2014 Jorik Tangelder <j.tangelder@gmail.com>;
- * Licensed under the MIT license */
+
 
 (function(window, undefined) {
   'use strict';
