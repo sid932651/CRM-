e5c80a1c080f4195811ce981977c53e875ec64c8
sid932651 <163431762+sid932651@users.noreply.github.com>
2024-04-04 20:50:57 +0530

    Update jquery.mousewheel.js

--- Mapplic/js/jquery.mousewheel.js
+++ Mapplic/js/jquery.mousewheel.js
@@ -1,14 +1,4 @@
-/*! Copyright (c) 2013 Brandon Aaron (http://brandonaaron.net)
- * Licensed under the MIT License (LICENSE.txt).
- *
- * Thanks to: http://adomas.org/javascript-mouse-wheel/ for some pointers.
- * Thanks to: Mathias Bank(http://www.mathias-bank.de) for a scope bug fix.
- * Thanks to: Seamus Leahy for adding deltaX and deltaY
- *
- * Version: 3.1.3
- *
- * Requires: 1.2.2+
- */
+
 
 (function (factory) {
 	if ( typeof define === 'function' && define.amd ) {
